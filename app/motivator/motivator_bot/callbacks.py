906e25244b0d69e510cd8e7f36f04300c82d8917
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2020-01-23 12:38:13 +0300

    started restyling

--- app/motivator/motivator_bot/callbacks.py
+++ app/motivator/motivator_bot/callbacks.py
@@ -6,8 +6,8 @@ from telegram.ext import CallbackContext, ConversationHandler
 from app.motivator.users.user_controller import UserController
 from app.motivator.users.bot_users import NewBotUser, KnownBotUser
 from app.motivator.habits.base import Habit
-from app.motivator.motivator_bot.presenters import greet, show_habits, cancellation_goodbye, confirm_choice, start_goodbye
-from app.motivator.motivator_bot.constants import REACT_START_CHOICE, REACT_HABIT_CHOICE
+from app.motivator.motivator_bot.presenters import TelegramPresenter, StartPresenter, show_habits, cancellation_goodbye, confirm_choice
+from app.motivator.motivator_bot.constants import REACT_HABIT_CHOICE
 from app.motivator.motivator_bot.utils import is_not_affirmative_choice
 
 
@@ -18,16 +18,10 @@ def start(update: Update, context: CallbackContext) -> int:
     Greets user and asks whether he is ready to add habits, if user
     has adding ability.
     """
-    user_id: int = update.message.chat.id
-    bot_user: Union[NewBotUser, KnownBotUser] = UserController.get_user(user_id)
-    context.user_data['bot_user'] = bot_user
 
-    if bot_user.can_start():
-        greet(update, bot_user)
-        return REACT_START_CHOICE
-    else:
-        start_goodbye(update)
-        return ConversationHandler.END
+    start_presenter: TelegramPresenter = StartPresenter(update, context)
+    start_presenter.run_presenter()
+    return start_presenter.get_next_state()
 
 
 def react_start_choice(update: Update, context: CallbackContext) -> int:
--- app/motivator/motivator_bot/presenters.py
+++ app/motivator/motivator_bot/presenters.py
@@ -1,32 +1,86 @@
 from typing import Union, List
+from abc import ABCMeta, abstractmethod
 
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import CallbackContext, ConversationHandler
 
+from app.motivator.users.user_controller import UserController
 from app.motivator.users.bot_users import NewBotUser, KnownBotUser
 from app.motivator.motivator_bot import constants
 
 
-# todo clean it! May be with abstract Presenter/
-def greet(update: Update, user: Union[NewBotUser, KnownBotUser]):
-    user_answer_choices: List[List[str]] = [constants.READY_TO_START_ANSWERS]
-    reply_keyboard = ReplyKeyboardMarkup(user_answer_choices, one_time_keyboard=True)
-    update.message.reply_text(
-        text=_get_typed_user_greet_message(user),
-        reply_markup=reply_keyboard
-    )
+class TelegramPresenter(metaclass=ABCMeta):
+    def __init__(self, update: Update, context: CallbackContext = None):
+        self._update = update
+        self._context = context
+
+    def run_presenter(self):
+        self._handle_update_data()
+        self._response()
+
+    @abstractmethod
+    def _handle_update_data(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _response(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_next_state(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def reply_keyboard(self) -> Union[ReplyKeyboardRemove, ReplyKeyboardMarkup]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def text(self) -> str:
+        raise NotImplementedError
 
 
-def start_goodbye(update: Update):
-    update.message.reply_text(text=constants.CANT_START_GREETING)
+class StartPresenter(TelegramPresenter):
+    def __init__(self, update: Update, context: CallbackContext):
+        super().__init__(update, context)
+        self._user_answer_choices: List[List[str]] = [constants.READY_TO_START_ANSWERS]
+        self._user_context_data: dict = self._context.user_data
 
+    def _handle_update_data(self):
+        user_id: int = self._update.message.chat.id
+        bot_user: Union[NewBotUser, KnownBotUser] = UserController.get_user(user_id)
+        self._user_context_data['bot_user_class']: Union[NewBotUser, KnownBotUser] = bot_user
+        self._user_context_data['user_can_start']: bool = bot_user.can_start()
 
-def _get_typed_user_greet_message(user: Union[NewBotUser, KnownBotUser]) -> str:
-    """todo potentially excessive. what if there are multiple user types? The only solution I saw was storing greet
-        message in User class, but it's does not suit logic
-    """
-    if isinstance(user, NewBotUser):
-        return constants.NEW_USER_GREETING
-    return constants.KNOWN_USER_GREETING
+    def _response(self):
+        self._update.message.reply_text(
+            text=self.text,
+            reply_markup=self.reply_keyboard
+        )
+
+    @property
+    def text(self) -> str:
+        if self._user_context_data['user_can_start']:
+            return self._get_typed_user_greet_message()
+        return constants.CANT_START_GREETING
+
+    def _get_typed_user_greet_message(self) -> str:
+        """todo potentially excessive. what if there are multiple user types? The only solution I saw was storing greet
+            message in User class, but it's does not suit logic
+        """
+        if isinstance(self._user_context_data['bot_user_class'], NewBotUser):
+            return constants.NEW_USER_GREETING
+        return constants.KNOWN_USER_GREETING
+
+    @property
+    def reply_keyboard(self) -> Union[ReplyKeyboardRemove, ReplyKeyboardMarkup]:
+        if self._user_context_data['user_can_start']:
+            return ReplyKeyboardMarkup(self._user_answer_choices, one_time_keyboard=True)
+        return ReplyKeyboardRemove()
+
+    def get_next_state(self) -> int:
+        if self._user_context_data['user_can_start']:
+            return constants.REACT_START_CHOICE
+        return ConversationHandler.END
 
 
 def cancellation_goodbye(update: Update):
