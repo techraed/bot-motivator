e0dcd4b0b91c0dd204be4a09c7ae512fb8ccc74c
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2020-02-05 16:19:43 +0300

    removed

--- tests/start_test.py
+++ tests/start_test.py
@@ -1,19 +1,6 @@
 import unittest
 
-from app.app_data.user_data_manager import UserDataManager
-from app.app_data.app_data_manager import TestDataManager
-from app.motivator.users.user_controller import UserController
-from app.motivator.users.bot_users import NewBotUser, KnownBotUser
 from tests.test_utils.start_test_utils import create_test_db_file, delete_test_db_file
-
-
-class TestUserDataManager(UserDataManager):
-    def __init__(self):
-        self._db_manager = TestDataManager()
-
-
-class TestUserController(UserController):
-    _user_data_manager = TestUserDataManager()
 
 
 class StartHandlerTest(unittest.TestCase):
@@ -21,14 +8,6 @@ class StartHandlerTest(unittest.TestCase):
     def setUp(self):
         self.user_id = 123
         create_test_db_file()
-
-    def test_user_types(self):
-        new_user = TestUserController.get_user(self.user_id)
-        self.assertEqual(isinstance(new_user, NewBotUser), True)
-
-        TestUserController.save_user(new_user)
-        known_user = TestUserController.get_user(self.user_id)
-        self.assertEqual(isinstance(known_user, KnownBotUser), True)
 
     def tearDown(self):
         delete_test_db_file()
