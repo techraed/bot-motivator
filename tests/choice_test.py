c14c2fd7ce16f02fead62b68a5288099dc49c42d
ziganshin1 <58744131+ziganshin1@users.noreply.github.com>
2020-02-25 22:56:52 +0300

    changed and finished choice test

--- tests/choice_test.py
+++ tests/choice_test.py
@@ -9,16 +9,19 @@ from tests.test_utils.start_test_utils import create_test_db_file, delete_test_d
 class ChoiceHandlerTest(unittest.TestCase):
     def setUp(self):
         self.user_id = 123
+        self.key = 'habits'
         create_test_db_file()
 
     def test_adding_data(self):
-        chosen_habit: str = "Перестать есть мучное"
         user_data: dict = TestUserDataManager().get_user_data(self.user_id)
         bot_user: Union[NewBotUser, KnownBotUser] = UserBuilder(self.user_id, user_data).build_user()
+        self.assertNotIn(self.key, user_data)
+
+        chosen_habit: str = "Перестать есть мучное"
         bot_user.add_habit(chosen_habit)
         TestUserDataManager().update_users_data(bot_user.user_data_for_save)
         user_data_new: dict = TestUserDataManager().get_user_data(self.user_id)
-        self.assertEqual(user_data_new['habits'][0]['habit_name'], chosen_habit)
+        self.assertIn(self.key, user_data_new)
 
     def tearDown(self):
         delete_test_db_file()
