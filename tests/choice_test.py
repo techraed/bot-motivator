940721c8fdbeb3533b23613cb30e1fbdac4f5bf0
ziganshin1 <58744131+ziganshin1@users.noreply.github.com>
2020-02-26 22:56:09 +0300

    add new choice test

--- tests/choice_test.py
+++ tests/choice_test.py
@@ -9,19 +9,18 @@ from tests.test_utils.start_test_utils import create_test_db_file, delete_test_d
 class ChoiceHandlerTest(unittest.TestCase):
     def setUp(self):
         self.user_id = 123
-        self.key = 'habits'
+        self.valid_data = ['message_state', 'motivational_messages', 'motivational_stickers', 'time_state']
         create_test_db_file()
 
     def test_adding_data(self):
         user_data: dict = TestUserDataManager().get_user_data(self.user_id)
         bot_user: Union[NewBotUser, KnownBotUser] = UserBuilder(self.user_id, user_data).build_user()
-        self.assertNotIn(self.key, user_data)
-
         chosen_habit: str = "Перестать есть мучное"
         bot_user.add_habit(chosen_habit)
         TestUserDataManager().update_users_data(bot_user.user_data_for_save)
         user_data_new: dict = TestUserDataManager().get_user_data(self.user_id)
-        self.assertIn(self.key, user_data_new)
+        for i in self.valid_data:
+            self.assertIn(i, user_data_new['habits'][0])
 
     def tearDown(self):
         delete_test_db_file()
