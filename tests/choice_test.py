9b23ecd9331c392efde737bb496aa1f24c91d731
ziganshin1 <58744131+ziganshin1@users.noreply.github.com>
2020-02-28 11:56:13 +0300

    add new final choice test

--- tests/choice_test.py
+++ tests/choice_test.py
@@ -13,28 +13,28 @@ class ChoiceHandlerTest(unittest.TestCase):
         self.key = 'habits'
         self.now_time: datetime = datetime.now()
         self.valid_data = [{'habit_name': 'Перестать есть мучное',
-                                              'message_state': 1,
-                                              'motivational_messages': {
-                                                   1: "'Жиртрест!' - так тебя будут называть через "
-                                                      'несколько лет! Перестань есть мучное уже.',
-                                                   2: 'Прошлое сообщения тебя взбудоражило, кажется. '
-                                                      'Оно и хорошо, ведь ты уже несколько дней '
-                                                      'держишься без мучного.',
-                                                   3: 'Слушай, а ты молодец! Тебе точно надо ачивки '
-                                                      "давать! Твоя первая ачивка - 'Сильная воля'.",
-                                                   4: 'Ну вот и кончилось мое воображение.'},
-                                              'motivational_stickers': {
-                                                   1: 'CAACAgIAAxkBAAIDcl5ATIFj3G5mlHYAAUnqrQy1WexR0gACewIAAgk7OxNZgdzEcocsihgE',
-                                                   2: 'CAACAgIAAxkBAAIDhF5AT7Hx_a7LBliztr-1sqXavM90AAJSAgACCTs7E46JbCIHDIpfGAQ',
-                                                   3: 'CAACAgIAAxkBAAIDg15AT3PZ9B9_7PrwsFYvZO3458-0AAJuAgACCTs7E2tp7UhrBKbWGAQ',
-                                                   4: 'CAACAgIAAxkBAAIDhV5AT-pbLJJxPr7qGBWaAUS5bChgAAKCAgACCTs7Extmv-eYI2c_GAQ'},
-                                              "time_state": {
-                                                     "year": self.now_time.year,
-                                                     "month": self.now_time.month,
-                                                     "day": self.now_time.day,
-                                                     "hour": self.now_time.hour,
-                                                     "minute": self.now_time.minute,
-                                                     "second": self.now_time.second}}]
+                            'message_state': 1,
+                            'motivational_messages': {
+                                1: "'Жиртрест!' - так тебя будут называть через "
+                                   'несколько лет! Перестань есть мучное уже.',
+                                2: 'Прошлое сообщения тебя взбудоражило, кажется. '
+                                   'Оно и хорошо, ведь ты уже несколько дней '
+                                   'держишься без мучного.',
+                                3: 'Слушай, а ты молодец! Тебе точно надо ачивки '
+                                   "давать! Твоя первая ачивка - 'Сильная воля'.",
+                                4: 'Ну вот и кончилось мое воображение.'},
+                            'motivational_stickers': {
+                                1: 'CAACAgIAAxkBAAIDcl5ATIFj3G5mlHYAAUnqrQy1WexR0gACewIAAgk7OxNZgdzEcocsihgE',
+                                2: 'CAACAgIAAxkBAAIDhF5AT7Hx_a7LBliztr-1sqXavM90AAJSAgACCTs7E46JbCIHDIpfGAQ',
+                                3: 'CAACAgIAAxkBAAIDg15AT3PZ9B9_7PrwsFYvZO3458-0AAJuAgACCTs7E2tp7UhrBKbWGAQ',
+                                4: 'CAACAgIAAxkBAAIDhV5AT-pbLJJxPr7qGBWaAUS5bChgAAKCAgACCTs7Extmv-eYI2c_GAQ'},
+                            "time_state": {
+                                "year": self.now_time.year,
+                                "month": self.now_time.month,
+                                "day": self.now_time.day,
+                                "hour": self.now_time.hour,
+                                "minute": self.now_time.minute,
+                                "second": self.now_time.second}}]
         create_test_db_file()
 
     def test_adding_data(self):
@@ -48,7 +48,7 @@ class ChoiceHandlerTest(unittest.TestCase):
 
     def tearDown(self):
         delete_test_db_file()
-        
+
 
 if __name__ == '__main__':
     unittest.main()
